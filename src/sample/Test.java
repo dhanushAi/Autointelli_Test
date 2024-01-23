ebbc031db64fbe495f9e00bfe640f37d61b4a7a5
dhanushAi <dhanush18021999@gmail.com>
2024-01-23 11:48:29 +0530

    sample

---
+++ src/sample/Test.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+public static void main(String[] args) throws InterruptedException {
+
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.facebook.com/login/");
+	Thread.sleep(3000);
+	driver.quit();
+	
+	System.out.println("Executed Successfully");
+}
+}
